ea2daaaae3a9b32f7d6094c10f449b7be290ea24
ViniciusLeitedoCarmo <87040226+ViniciusLeitedoCarmo@users.noreply.github.com>
2021-09-05 19:18:51 -0300

    Main file

---
+++ leapyorn.py
@@ -0,0 +1,10 @@
+print("Let's find out if a year is leap or not?")
+calculate="Y"
+while calculate=="Y":
+  ano=int(input("Type the year:"))
+  if ano%400==0 or ano%4==0 and ano%100!=0:
+    print("It's Leap")
+  else:
+    print("Not leap.")
+  calcular=input("Want use again?(Y/N)")
+print("End")
